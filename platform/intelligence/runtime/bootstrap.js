92cdbe659e1e4c7d2b8477ed38de78a2430ceae0
Jorge Palacios <jorgepalaciosrodriguez@outlook.com>
2026-07-21 13:45:37 -0600

    refactor(fir): centralize runtime bootstrap

--- platform/intelligence/runtime/bootstrap.js
+++ platform/intelligence/runtime/bootstrap.js
@@ -1,6 +1,14 @@
+import { ForgeIntelligenceRuntime } from "./ForgeIntelligenceRuntime.js";
+
 import { IntelligenceRegistry } from "../registry/IntelligenceRegistry.js";
+import { IntelligenceRouter } from "../router/IntelligenceRouter.js";
+
+import { ContextBuilder } from "../context/ContextBuilder.js";
+import { AuthorityGate } from "../authority/AuthorityGate.js";
+
 import { ProviderManager } from "../providers/ProviderManager.js";
 import { MockProvider } from "../providers/MockProvider.js";
+
 import { AlfredIntelligence } from "../intelligences/AlfredIntelligence.js";
 
 export const providerManager = new ProviderManager();
@@ -8,5 +16,25 @@ providerManager.register("mock", new MockProvider());
 
 export const registry = new IntelligenceRegistry();
 
-registry.register("default", new AlfredIntelligence(providerManager));
-registry.register("alfred", new AlfredIntelligence(providerManager));
+registry.register(
+  "default",
+  new AlfredIntelligence(providerManager)
+);
+
+registry.register(
+  "alfred",
+  new AlfredIntelligence(providerManager)
+);
+
+export const router = new IntelligenceRouter(registry);
+
+export const contextBuilder = new ContextBuilder();
+
+export const authorityGate = new AuthorityGate();
+
+export const runtime = new ForgeIntelligenceRuntime({
+  registry,
+  router,
+  contextBuilder,
+  authorityGate
+});
--- platform/intelligence/tests/runtime-smoke-test.js
+++ platform/intelligence/tests/runtime-smoke-test.js
@@ -1,25 +1,9 @@
-import { ForgeIntelligenceRuntime } from "../runtime/ForgeIntelligenceRuntime.js";
-import { IntelligenceRegistry } from "../registry/IntelligenceRegistry.js";
-import { IntelligenceRouter } from "../router/IntelligenceRouter.js";
-import { ContextBuilder } from "../context/ContextBuilder.js";
-import { AuthorityGate } from "../authority/AuthorityGate.js";
-import { EchoIntelligence } from "../intelligences/EchoIntelligence.js";
+import { runtime } from "../runtime/bootstrap.js";
 import { IntelligenceRequest } from "../contracts/IntelligenceRequest.js";
-
-const registry = new IntelligenceRegistry();
-registry.register("alfred", new EchoIntelligence());
-registry.register("default", new EchoIntelligence());
-
-const runtime = new ForgeIntelligenceRuntime({
-  registry,
-  router: new IntelligenceRouter(registry),
-  contextBuilder: new ContextBuilder(),
-  authorityGate: new AuthorityGate()
-});
 
 const result = await runtime.execute(
   new IntelligenceRequest({
-    message: "Hello ProviderManager",
+    message: "Bootstrap Online",
     metadata: {
       intent: "alfred"
     }
