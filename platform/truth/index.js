ef6100108f41a80df37036551d45bcfc1b71a653
Jorge Palacios <jorgepalaciosrodriguez@outlook.com>
2026-07-21 20:34:11 -0600

    cleanup(wave-10): remove inherited src cluster

--- platform/truth/index.js
+++
@@ -1,5 +0,0 @@
-export * from './contracts/truth-types.js';
-export * from './contracts/evidence-states.js';
-export * from './contracts/owner-types.js';
-export * from './contracts/truth-envelope-required-fields.js';
-export * from './validators/index.js';
